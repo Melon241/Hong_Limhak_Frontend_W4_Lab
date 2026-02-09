142fdf0bb8a2add6178ece349f91f905dc021875
Melon241 <141200850+Melon241@users.noreply.github.com>
2026-02-10 00:04:50 +0700

    Submit

--- exercise-4/src/App.jsx
+++ exercise-4/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* Contants used in this component */
+  const DOLLAR_TO_EURO_RATIO = 0.92;
+  const DOLLAR_TO_DONG_RATIO = 24560;
+
+  let valueDollars = 15;
+
+  // Convert  to euro
+  function dollarToEuro(valueInDollars) {
+    return valueInDollars * DOLLAR_TO_EURO_RATIO;
+  }
+
+  
+  // Convert to dong
+  function dollarToDong(valueInDollars) {
+    return valueInDollars * DOLLAR_TO_DONG_RATIO;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <main>
+      <h1>Device conversions</h1>
+
+      <p>
+        <label>Current value in dollars</label>
+        <input disabled value={valueDollars} />
+
+        <label>Value in Dong</label>
+        <input disabled value={dollarToDong(valueDollars)} />
+
+        <label>Value in Euro</label>
+        <input disabled value={dollarToEuro(valueDollars)} />
       </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
